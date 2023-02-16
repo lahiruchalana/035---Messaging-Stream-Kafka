fa659c9078a9e87599569bc99d02f76204d3af34
Lahiru Chalana <lahiruchalana@incubatelabs.com>
2023-02-16 15:41:44 +0530

    Completed Kafka Consumer

--- src/main/java/com/example/messagingservice/MessagingServiceApplication.java
+++ src/main/java/com/example/messagingservice/MessagingServiceApplication.java
@@ -16,7 +16,7 @@ public class MessagingServiceApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-            for (int i = 0; i < 10_000; i++) {
+            for (int i = 0; i < 100; i++) {
                 kafkaTemplate.send("message", "Hey Kafka bro!!! " + i);
             }
         };
---
+++ src/main/java/com/example/messagingservice/controller/MessageController.java
@@ -0,0 +1,4 @@
+package com.example.messagingservice.controller;
+
+public class MessageController {
+}
